6ed89d2850f19b38e08826a84529893b6a32a2a7
Andrew <andrewsmiththefirst@gmail.com>
2018-11-13 20:21:22 -0500

    added user payload returns

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
+
 //Load user model
 const User = require("../../models/User");
 // @route GET api/users/test
@@ -85,5 +87,20 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route GET api/users/current
+// @desc Return current User
+// @access Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
